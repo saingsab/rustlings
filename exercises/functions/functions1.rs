6d962693094ce2a5e3cfac844c52072351ac20c7
Ayoung <saing.sab@gmail.com>
2023-12-29 15:53:39 +0700

    excer Variables, Functions, IF

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,7 +3,9 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+fn call_me() -> String {
+    String::from("Hello World!")
+}
 
 fn main() {
     call_me();
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,9 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples() -> u64 {
+    35
+}
 
 // Don't modify this function!
 #[test]
