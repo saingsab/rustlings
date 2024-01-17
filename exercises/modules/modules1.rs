9af1ca2f24ea3dbc83f98357db2ae5da05f0a0c3
Ayoung <saing.sab@gmail.com>
2024-01-17 23:15:12 +0700

    modules1

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,16 +3,17 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-mod sausage_factory {
+use crate::sausage_factory::get_secret_recipe;
+
+pub mod sausage_factory {
     // Don't let anybody outside of this module see this!
-    fn get_secret_recipe() -> String {
+    pub fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
-        get_secret_recipe();
+    pub fn make_sausage() {
+        crate::get_secret_recipe();
         println!("sausage!");
     }
 }
